2a7d58e27fb7359dc21311e03d0a5c8e257404fc
trianasalsabila <73945435+trianasalsabila@users.noreply.github.com>
2020-12-18 08:00:46 +0700

    Update Program System Informasi Bank Sampah.py

--- Program System Informasi Bank Sampah.py
+++ Program System Informasi Bank Sampah.py
@@ -1,1 +1,39 @@
 # Algoritma-dan-Pemrograman-Program-System-Informasi-Bank-Sampah
+#program Triana Salsabila
+#Program Bank Sampah
+
+import os
+
+def clearscreen():
+    if os.name == "posix":
+        os.system("clear")
+    else:
+        os.system("cls")
+        
+def mainMenu():
+    x = ""
+    while x != "0":
+        print("Program Sistem Informasi Bank Sampah")
+        print("[1] Tampilkan Buku Tabungan")
+        print("[0] Logout")
+        print()
+        
+        x = input("Opt : ")
+        clearscreen()
+        
+        if x == "1":
+            BukuTabungan()
+            
+        elif x == "0":
+            print("Logging Out...")
+            print()
+            
+        elif x == "2":
+            tabung()
+            
+        elif x == "Admin":
+            admin()
+         
+        else:
+            clearscreen("Option not found.")
+            print()
